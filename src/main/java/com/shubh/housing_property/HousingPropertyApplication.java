3b631599d78202f1704dbed878b05d20ebc5db50
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-01-11 14:46:46 +0530

    first commit

---
+++ src/main/java/com/shubh/housing_property/HousingPropertyApplication.java
@@ -0,0 +1,13 @@
+package com.shubh.housing_property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingPropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingPropertyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubh/housing_property/HousingPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubh.housing_property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingPropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
