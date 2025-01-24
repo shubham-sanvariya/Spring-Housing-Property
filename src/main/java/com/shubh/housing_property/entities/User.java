d5566e43488ec65088552b8217ac2ca2d33540c8
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-01-24 18:21:39 +0530

    adding user entity

---
+++ src/main/java/com/shubh/housing_property/entities/User.java
@@ -0,0 +1,43 @@
+package com.shubh.housing_property.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    private boolean verified;
+
+    @Column
+    private String otp;
+
+    @Column
+    private LocalDateTime otpGeneratedTime;
+}
