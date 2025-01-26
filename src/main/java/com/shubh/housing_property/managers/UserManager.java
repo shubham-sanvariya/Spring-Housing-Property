bb4d909bc3b760157bd33ea907b2c4f741024448
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-01-26 20:28:05 +0530

    adding user manager component

---
+++ src/main/java/com/shubh/housing_property/managers/UserManager.java
@@ -0,0 +1,23 @@
+package com.shubh.housing_property.managers;
+
+import org.springframework.stereotype.Component;
+
+import com.shubh.housing_property.dtos.UserDTO;
+import com.shubh.housing_property.entities.User;
+import com.shubh.housing_property.mappers.UserMapper;
+import com.shubh.housing_property.repos.UserRepo;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class UserManager {
+
+    private UserRepo userRepo;
+
+    public UserDTO getByEmail(String email){
+        User user = userRepo.findByEmail(email).orElse(null);
+
+        return UserMapper.INSTANCE.toUserDTO(user);
+    }
+}
