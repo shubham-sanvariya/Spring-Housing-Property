c4cc52af952bad247a90de5e290cafee559ba261
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-01-26 17:59:53 +0530

    adding find by email method in user repo

--- src/main/java/com/shubh/housing_property/repos/UserRepo.java
+++ src/main/java/com/shubh/housing_property/repos/UserRepo.java
@@ -3,7 +3,9 @@ package com.shubh.housing_property.repos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shubh.housing_property.entities.User;
+import java.util.Optional;
+
 
 public interface UserRepo extends JpaRepository<User,Long>{
-    
+    Optional<User> findByEmail(String email);
 }
