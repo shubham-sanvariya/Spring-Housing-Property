df00ba82dfdc20c939fd30510fae8971aae2f17f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-01-26 20:24:12 +0530

    adding mapper for user dto

--- src/main/java/com/shubh/housing_property/mappers/UserMapper.java
+++ src/main/java/com/shubh/housing_property/mappers/UserMapper.java
@@ -5,11 +5,15 @@ import org.mapstruct.factory.Mappers;
 
 import com.shubh.housing_property.dtos.UserDTO;
 import com.shubh.housing_property.entities.User;
+import com.shubh.housing_property.request.UserRequest;
 
 @Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     User toUser(UserDTO userDTO);
-    
+
+    UserDTO toUserDTO(User user);
+
+    UserDTO toUserDTO(UserRequest userRequest);
 }
