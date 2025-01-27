1584a980c3de64e5fbb9104008887f1410357f7d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-01-27 14:57:27 +0530

    adding verify otp method in user service

--- src/main/java/com/shubh/housing_property/services/Userservice.java
+++ src/main/java/com/shubh/housing_property/services/Userservice.java
@@ -3,6 +3,7 @@ package com.shubh.housing_property.services;
 import java.time.LocalDateTime;
 import java.util.Random;
 
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ import com.shubh.housing_property.dtos.UserDTO;
 import com.shubh.housing_property.managers.UserManager;
 import com.shubh.housing_property.mappers.UserMapper;
 import com.shubh.housing_property.request.UserRequest;
+import com.shubh.housing_property.response.BaseResponse;
 import com.shubh.housing_property.utils.EmailUtility;
 import com.shubh.housing_property.utils.JwtUtility;
 
@@ -18,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class Userservice {
-    
+
     private UserManager userManager;
 
     private EmailUtility emailUtility;
@@ -27,7 +29,7 @@ public class Userservice {
 
     private JwtUtility jwtUtility;
 
-    public String signup(UserRequest userRequest){
+    public BaseResponse signup(UserRequest userRequest) {
         UserDTO userDTO = userManager.getByEmail(userRequest.getEmail());
 
         if (userDTO != null) {
@@ -45,6 +47,30 @@ public class Userservice {
 
         emailUtility.sendEmail(userDTO.getEmail(), "Email Verification", "Your OTP is: " + otp);
 
-        return "Signup successfull Please verify your email.";
+        return new BaseResponse(200, "Signup successful! Please verify your email.");
+    }
+
+    public BaseResponse verifyOtp(String email, String otp) {
+        UserDTO userDTO = userManager.getByEmail(email);
+
+        if (userDTO == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        if (userDTO.getOtp().equals(otp)) {
+            if (userDTO.getOtpGeneratedTime().isBefore(LocalDateTime.now().minusMinutes(10))) {
+                throw new IllegalArgumentException("OTP has expired.");
+            }
+
+            userDTO.setVerified(true);
+            userDTO.setOtp(null);
+            userDTO.setOtpGeneratedTime(null);
+
+            userManager.save(userDTO);
+
+            return new BaseResponse(200, "Email verified successfully!");
+        }else{
+            throw new IllegalArgumentException("Invalid OTP.");
+        }
     }
 }
