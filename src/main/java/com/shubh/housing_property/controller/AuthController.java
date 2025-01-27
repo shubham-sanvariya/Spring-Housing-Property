65bea3c7d05f99087b549f2286388002adad6622
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-01-27 15:16:41 +0530

    adding verify otp method in auth controller

--- src/main/java/com/shubh/housing_property/controller/AuthController.java
+++ src/main/java/com/shubh/housing_property/controller/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shubh.housing_property.request.UserRequest;
@@ -22,6 +23,12 @@ public class AuthController {
     @PostMapping("/signup")
     public ResponseEntity<BaseResponse> signup(@RequestBody UserRequest userRequest){
         BaseResponse res = userservice.signup(userRequest);
+        return ResponseEntity.ok(res);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<BaseResponse> verifyOtp(@RequestParam String email, @RequestParam String otp){
+        BaseResponse res = userservice.verifyOtp(email, otp);
         return ResponseEntity.ok(res);
     }
 }
