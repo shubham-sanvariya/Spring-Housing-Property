632073c0e27078500c9d4e19d045787a89bd7ad2
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-01-27 15:13:30 +0530

    adding signup method in auth controller

---
+++ src/main/java/com/shubh/housing_property/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.shubh.housing_property.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shubh.housing_property.request.UserRequest;
+import com.shubh.housing_property.response.BaseResponse;
+import com.shubh.housing_property.services.Userservice;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    
+    private Userservice userservice;
+
+    @PostMapping("/signup")
+    public ResponseEntity<BaseResponse> signup(@RequestBody UserRequest userRequest){
+        BaseResponse res = userservice.signup(userRequest);
+        return ResponseEntity.ok(res);
+    }
+}
