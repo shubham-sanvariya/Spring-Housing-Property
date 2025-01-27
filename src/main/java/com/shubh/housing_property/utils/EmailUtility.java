722f1ae85aa058ab12a37c3a47933f8999f85133
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-01-27 14:26:13 +0530

    adding email utils

---
+++ src/main/java/com/shubh/housing_property/utils/EmailUtility.java
@@ -0,0 +1,22 @@
+package com.shubh.housing_property.utils;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class EmailUtility {
+    
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
